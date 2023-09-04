8e0039108e45bda80f14949e7d9e3ea4066ac548
Yoong <kjr928721@gmail.com>
2023-09-04 11:04:28 +0900

    23.09.04

--- ccnets_api_main/ccnets/config.py
+++ ccnets_api_main/ccnets/config.py
@@ -7,14 +7,14 @@ import json
 from argparse import ArgumentParser
 
 def write_parser(args):
-    with open("../ccnets/config.txt","w") as f:
+    with open("./ccnets_api_main/ccnets/config.txt","w") as f:
         json.dump(args.__dict__, f, indent=2)
     f.close()
 
 def get_parser():
     parser = ArgumentParser()
     args = parser.parse_args(args = [])
-    with open("../ccnets/config.txt", 'r') as f:
+    with open("./ccnets_api_main/ccnets/config.txt", 'r') as f:
         args.__dict__ = json.load(f)
     f.close()
     return args
